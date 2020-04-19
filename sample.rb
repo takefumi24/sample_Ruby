e0ddbf9ed307443902db3ea8b631722885edef80
takefumi24 <t_ohba24@yahoo.co.jp>
2020-04-20 02:57:45 +0900

    Update sample.rb

--- sample.rb
+++ sample.rb
@@ -1,16 +1,6 @@
-def decryption(char)
-  # char_ary = "frqjudwxodwlrq"
-  changed_char_ary = []#空の配列を用意する
-  char.chars.each do |char|
-    changed_char_ary << (char.ord - 3).chr
-     #ordメソッドで整数にしchrメソッドで文字に変換する
-      #空の配列に入れる
-  end
-  puts changed_char_ary.join
-     # =>edjdhoa
-     #joinメソッドで配列を連結して文字列にする
+languages = ["日本語", "英語", "スペイン語"]
+
+# each文を用いて、要素ごとに「○○を話せます」と出力してください
+languages.each do |language|
+  puts "#{language}を話せます"
 end
-
-
-char= "frqjudwxodwlrq"
-decryption(char)
