e02fe3526588aa3f30af1a9ed1c95e3f1e135981
takefumi24 <t_ohba24@yahoo.co.jp>
2020-04-20 03:02:57 +0900

    border.commit

--- sample.rb
+++ sample.rb
@@ -1,6 +1,8 @@
 languages = ["日本語", "英語", "スペイン語"]
+border = "---------------------"
 
-# each文を用いて、要素ごとに「○○を話せます」と出力してください
 languages.each do |language|
+  # 変数borderを出力してください
+  puts border
   puts "#{language}を話せます"
 end
