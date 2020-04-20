26bbe60fe2f4c67e2cc507573bdc306c922c9a0c
takefumi24 <t_ohba24@yahoo.co.jp>
2020-04-21 01:39:47 +0900

    ddddddd

--- sample.rb
+++ sample.rb
@@ -1,18 +1,1 @@
-characters = [
-  {name: "にんじゃわんこ", age: 14},
-  {name: "ひつじ仙人"},
-  {name: "ベイビーわんこ", age: 5},
-  {name: "とりずきん"}
-]
-
-characters.each do |character|
-  puts "--------------------"
-  puts "名前は#{character[:name]}です"
-
-  # キー:ageの値に応じて年齢の情報を出力してください
-  if character[:age]
-    puts "年齢は#{character[:age]}歳です"
-  else
-    puts"年齢は秘密です"
-  end
-end
+character,.sfs
