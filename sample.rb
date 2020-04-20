6ccea79d4d466baee0ee8ceb957c21d31834cb32
takefumi24 <t_ohba24@yahoo.co.jp>
2020-04-20 23:59:59 +0900

    Rubyde

--- sample.rb
+++ sample.rb
@@ -1,4 +1,8 @@
-# 省略した書き方で書き換えてください
-exam = {subject:"Math", score:80}
+exam = {subject: "Math"}
 
-puts "#{exam[:subject]}: #{exam[:score]}点"
+# 条件式にキー「:score」の値を使うif文をつくってください
+if exam[:score]
+  puts "#{exam[:subject]}の結果は#{exam[:score]}点です"
+elsif
+  puts "#{exam[:subject]}の結果はわかりません"
+end
