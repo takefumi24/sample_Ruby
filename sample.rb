414194cd9a93245ad30db1257bff1e754dfe1e21
takefumi24 <t_ohba24@yahoo.co.jp>
2020-04-21 01:24:50 +0900

    commit

--- sample.rb
+++ sample.rb
@@ -3,5 +3,7 @@ exams = [
   {subject: "Science", score: 55}
 ]
 
-# インデックス番号が1の要素の、キーが「:score」の値を出力してください
-puts exams[1][:score]
+# each文を用いて、要素ごとに「○○の結果は△△点です」と出力してください
+exams.each do |exam|
+  puts "#{exam[:subject]}の結果は#{exam[:score]}点です"
+end
