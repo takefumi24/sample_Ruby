06ed9f83629866b6afc4cbfb70c708fa15732b1d
takefumi24 <t_ohba24@yahoo.co.jp>
2020-04-22 01:31:01 +0900

    getsメソッド

--- sample.rb
+++ sample.rb
@@ -1,6 +1,6 @@
-genre="映画"
-title= "時をかける少女"
-review="人生の最高傑作アニメ。\n\n青春厨としてはたまらない甘酸っぱい青春ストーリー。\n\nちあきのイケメンさは言うまでもない。\n\n人にお金を払ってでもみて欲しい作品。\n\n早く見たほうがいいよ。\nTime waits for no one."
+genre=gets
+title= gets
+review=gets
 dot_line="---------------------------"
 
 puts "ジャンル : #{genre}\n#{dot_line}"
