1d33e762ce33aa2233cb66735319e8f774c00a3a
takefumi24 <t_ohba24@yahoo.co.jp>
2020-04-22 01:23:44 +0900

    式展開

--- sample.rb
+++ sample.rb
@@ -3,9 +3,6 @@ title= "時をかける少女"
 review="人生の最高傑作アニメ。\n\n青春厨としてはたまらない甘酸っぱい青春ストーリー。\n\nちあきのイケメンさは言うまでもない。\n\n人にお金を払ってでもみて欲しい作品。\n\n早く見たほうがいいよ。\nTime waits for no one."
 dot_line="---------------------------"
 
-puts "ジャンル : " + genre
-puts dot_line
-puts "タイトル : " + title
-puts dot_line
-puts "感想 : " + review
-puts dot_line
+puts "ジャンル : #{genre}\n#{dot_line}"
+puts "タイトル : #{title}\n#{dot_line}"
+puts "感想 : #{review}\n#{dot_line}"
