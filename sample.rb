4d0b5acaa6e29e3035734b566ca3e3ee3799ee17
takefumi24 <t_ohba24@yahoo.co.jp>
2020-04-22 02:00:49 +0900

    ハッシュに変更

--- sample.rb
+++ sample.rb
@@ -1,4 +1,13 @@
-hash = {title: "時をかける少女"}
-puts hash[:title]
-hash[:title]="るろうに剣心"
-puts hash[:title]
+post = {}
+puts "ジャンルを入力してください："
+post[:genre] = gets.chomp
+puts "タイトルを入力してください："
+post[:title] = gets.chomp
+puts "感想を入力してください："
+post[:review] = gets.chomp
+line = "---------------------------"
+
+# レビューの描画
+puts "ジャンル : #{post[:genre]}\n#{line}"
+puts "タイトル : #{post[:title]}\n#{line}"
+puts "感想 :\n#{post[:review]}\n#{line}"
