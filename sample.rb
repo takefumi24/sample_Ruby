1d3afb284dfaaf42ee5e91dd02231f6d7ac3eab5
takefumi24 <t_ohba24@yahoo.co.jp>
2020-04-22 01:52:49 +0900

    ハッシュの使い方

--- sample.rb
+++ sample.rb
@@ -1,11 +1,4 @@
-puts "ジャンルを入力してください:"
-genre=gets.chomp
-puts "タイトルを入力してください:"
-title= gets.chomp
-puts "感想を入力してください:"
-review=gets.chomp
-dot_line="---------------------------"
-
-puts "ジャンル : #{genre}\n#{dot_line}"
-puts "タイトル : #{title}\n#{dot_line}"
-puts "感想 : \n#{review}\n#{dot_line}"
+hash = {title: "時をかける少女"}
+puts hash[:title]
+hash[:title]="るろうに剣心"
+puts hash[:title]
