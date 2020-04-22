f441fa80210771fe0be2e244ad8d0d6157f8f2da
takefumi24 <t_ohba24@yahoo.co.jp>
2020-04-22 12:24:02 +0900

    while文作成

--- sample.rb
+++ sample.rb
@@ -1,21 +1,44 @@
-puts "レビュー数:0"
-puts "[0]レビューを書く"
-puts "[1]レビューを読む"
-puts "[2]アプリを終了する"
-input=gets.chomp
+def post_review
+  post = {}
+  puts "ジャンルを入力してください："
+  post[:genre] = gets.chomp
+  puts "タイトルを入力してください："
+  post[:title] = gets.chomp
+  puts "感想を入力してください："
+  post[:review] = gets.chomp
+  line = "---------------------------"
 
+  # レビューの描画
+  puts "ジャンル : #{post[:genre]}\n#{line}"
+  puts "タイトル : #{post[:title]}\n#{line}"
+  puts "感想 : \n#{post[:review]}\n#{line}"
+end
+def read_review
 
+end
 
-post = {}
-puts "ジャンルを入力してください："
-post[:genre] = gets.chomp
-puts "タイトルを入力してください："
-post[:title] = gets.chomp
-puts "感想を入力してください："
-post[:review] = gets.chomp
-line = "---------------------------"
+def end_program
+  exit
+end
 
-# レビューの描画
-puts "ジャンル : #{post[:genre]}\n#{line}"
-puts "タイトル : #{post[:title]}\n#{line}"
-puts "感想 :\n#{post[:review]}\n#{line}"
+def exception
+  puts "入力された値は無効な値です"
+end
+
+while true do
+  puts "レビュー数:0"
+  puts "[0]レビューを書く"
+  puts "[1]レビューを読む"
+  puts "[2]アプリを終了する"
+  input=gets.to_i
+
+  if input==0 then
+    post_review
+  elsif input==1 then
+    read_review
+  elsif input==2 then
+    end_program
+  else
+    exception
+  end
+end
