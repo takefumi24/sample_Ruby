3c6412baa0aecfcf3098be2c9e1577a4d21e62c0
takefumi24 <t_ohba24@yahoo.co.jp>
2020-04-22 15:44:08 +0900

    お金を引き出すプログラム作成

--- sample.rb
+++ sample.rb
@@ -1,15 +1,14 @@
-profile={}
-profile[:name]="太郎"
-profile[:age]=26
-profile[:address]="高知"
-puts profile
+def withdraw(balance, amount)
+  fee = 110# 手数料
+  if (amount+fee)<=balance
+    balance-=(amount+fee)
+    puts "#{amount}円引き落としました。残高は#{balance}円です"
+  else
+    puts "残高不足です"
+  end
+end
 
-profiles=[]
-profiles<<profile
-
-profile2={}
-profile2[:name]="次郎"
-profile2[:age]=27
-profile2[:address]="愛媛"
-profiles<<profile2
-puts profiles
+balance = 100000# 残高
+puts "いくら引き落としますか？（手数料110円かかります）"
+money = gets.to_i
+withdraw(balance, money)
