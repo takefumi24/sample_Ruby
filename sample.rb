85814a9aad0a9c11f16c3e7ced4d37237b303333
takefumi24 <t_ohba24@yahoo.co.jp>
2020-04-22 15:25:00 +0900

    課題1,2

--- sample.rb
+++ sample.rb
@@ -1,61 +1,15 @@
-def post_review(a_posts)
-  post = {}
-  puts "ジャンルを入力してください："
-  post[:genre] = gets.chomp
-  puts "タイトルを入力してください："
-  post[:title] = gets.chomp
-  puts "感想を入力してください："
-  post[:review] = gets.chomp
-  line = "---------------------------"
+profile={}
+profile[:name]="太郎"
+profile[:age]=26
+profile[:address]="高知"
+puts profile
 
-  # レビューの描画
-  puts "ジャンル : #{post[:genre]}\n#{line}"
-  puts "タイトル : #{post[:title]}\n#{line}"
-  puts "感想 : \n#{post[:review]}\n#{line}"
+profiles=[]
+profiles<<profile
 
-  a_posts<<post
-  return a_posts
-end
-def read_reviews(a_posts)
-  number=0
-  a_posts.each do |post|
-    puts "[#{number}]:#{post[:title]}のレビュー"
-    number+=1
-  end
-  puts "見たいレビューの番号を入力してください"
-  input=gets.to_i
-  post=a_posts[input]
-  line="---------------------------"
-  puts "ジャンル : #{post[:genre]}\n#{line}"
-  puts "タイトル : #{post[:title]}\n#{line}"
-  puts "感想 : \n#{post[:review]}\n#{line}"
-
-end
-
-def end_program
-  exit
-end
-
-def exception
-  puts "入力された値は無効な値です"
-end
-
-posts = []
-while true do
-  puts "レビュー数 : #{posts.length}"
-  puts "[0]レビューを書く"
-  puts "[1]レビューを読む"
-  puts "[2]アプリを終了する"
-  input=gets.to_i
-
-  if input==0 then
-    posts=post_review(posts)
-  elsif input==1 then
-
-    read_reviews(posts)
-  elsif input==2 then
-    end_program
-  else
-    exception
-  end
-end
+profile2={}
+profile2[:name]="次郎"
+profile2[:age]=27
+profile2[:address]="愛媛"
+profiles<<profile2
+puts profiles
