d7562c3e38e7bd7b41fb3e03556191d1bd9cf671
takefumi24 <t_ohba24@yahoo.co.jp>
2020-04-25 15:19:13 +0900

    while文　if文

--- sample.rb
+++ sample.rb
@@ -1,12 +1,42 @@
-post={}
-puts "ジャンルを入力してください："
-post[:genre] = gets.chomp
-puts "タイトルを入力してください："
-post[:title] = gets.chomp
-puts "感想を入力してください："
-post[:review] = gets.chomp
-line = "---------------------------"
+def post_review
+  post={}
+  puts "ジャンルを入力してください："
+  post[:genre] = gets.chomp
+  puts "タイトルを入力してください："
+  post[:title] = gets.chomp
+  puts "感想を入力してください："
+  post[:review] = gets.chomp
+  line = "---------------------------"
 
-puts "ジャンル : #{post[:genre]}\n#{line}"
-puts "タイトル : #{post[:title]}\n#{line}"
-puts "感想 :\n#{post[:review]}\n#{line}"
+  puts "ジャンル : #{post[:genre]}\n#{line}"
+  puts "タイトル : #{post[:title]}\n#{line}"
+  puts "感想 :\n#{post[:review]}\n#{line}"
+end
+
+def read_review
+end
+
+def end_program
+  exit
+end
+
+def exception
+  puts "入力された値は無効な値です"
+end
+
+while true do
+  puts "レビュー数：0"
+  puts "[0]レビューを書く"
+  puts "[1]レビューを読む"
+  puts "[2]アプリを終了する"
+  input = gets.to_i
+  if input==0
+    post_review
+  elsif input==1
+    read_review
+  elsif input==2
+  end_program
+  else
+    puts "入力された値は、無効な値です"
+  end
+end
