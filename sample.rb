c5c212e3757ca782718fd66d9c7818d47dfc5657
takefumi24 <t_ohba24@yahoo.co.jp>
2020-04-25 16:22:44 +0900

    変数、レビュー一覧

--- sample.rb
+++ sample.rb
@@ -1,4 +1,4 @@
-def post_review
+def post_review(a_posts)
   post={}
   puts "ジャンルを入力してください："
   post[:genre] = gets.chomp
@@ -11,9 +11,19 @@ def post_review
   puts "ジャンル : #{post[:genre]}\n#{line}"
   puts "タイトル : #{post[:title]}\n#{line}"
   puts "感想 :\n#{post[:review]}\n#{line}"
+  a_posts<<post
+  return a_posts
 end
 
-def read_review
+def read_review(a_posts)
+  puts "見たいレビューの番号を入力してください："
+  post=a_posts[input]
+
+  line="---------------------------"
+  puts "ジャンル : #{post[:genre]}\n#{line}"
+  puts "タイトル : #{post[:title]}\n#{line}"
+  puts "感想 :\n#{post[:review]}\n#{line}"
+
 end
 
 def end_program
@@ -24,16 +34,18 @@ def exception
   puts "入力された値は無効な値です"
 end
 
+posts=[]
+
 while true do
-  puts "レビュー数：0"
+  puts "レビュー数：#{posts.length}"
   puts "[0]レビューを書く"
   puts "[1]レビューを読む"
   puts "[2]アプリを終了する"
   input = gets.to_i
   if input==0
-    post_review
+    posts=post_review(posts)
   elsif input==1
-    read_review
+    read_review(posts)
   elsif input==2
   end_program
   else
