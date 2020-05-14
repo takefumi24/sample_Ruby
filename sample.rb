4537c0c65132e2ea436c6b6b47ef628c68a23fbe
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-14 20:39:16 +0900

    squaredメソッド

--- sample.rb
+++ sample.rb
@@ -1,11 +1,8 @@
-def sleep_in(weekday, vacation)
-  if vacation == false
-    puts "False"
-  else
-    puts "True"
-  end
+def squared(num)
+  num ** 2
 end
 
-weekday = true
-vacation = false
-sleep_in(weekday, vacation)
+puts "一辺の長さを入力してください"
+
+side_length = gets.to_i
+puts "１辺の長さが#{side_length}cmの正方形の面積は#{squared(side_length)}㎠です"
