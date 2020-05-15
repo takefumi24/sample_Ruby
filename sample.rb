aa1fdc7ab4dfb8834213aa8ef6dd59f981ee3bf7
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-15 11:51:50 +0900

    別解(08)

--- sample.rb
+++ sample.rb
@@ -1,8 +1,33 @@
-name = "Suzuki"
-height = 1.6
-weight = 60
-bmi = (weight / height ** 2 ).round(1)
-culc = {name:name, height:height, weight:weight, bmi:bmi}
-culc.each do |key, value|
-puts "#{key}:#{value}"
-end
+  def rename(name)
+    name = "Mr.#{name}"
+  end
+
+  name = "Tanaka"
+  rename(name)
+  puts name
+
+# 上記のコードは、ターミナルに「Mr.Tanaka」と表記されることを意図して
+# 書いていますが、実際はそうなりません。
+
+# その理由を、「スコープ」という用語を用いて説明してください。
+
+# A,変数のスコープという概念があり、スコープ外の変数にはアクセスすることができない
+#   Rubyの場合はメソッドの内側と外側ではスコープが違うという仕様になっている。
+#   そのため「def」で定義されているnameという変数と、defの外側のnameは別物である。
+#   よって、renameメソッドのなかのnameに代入しても、外側のnameに変更はない。
+
+
+
+
+# コードを下記のように修正するとうまくいく理由を、「引数」「返り値」という用語を用いて説明してください。
+  def rename(name)
+    name = "Mr.#{name}"
+  en.
+
+  name = "Tanaka"
+  name = rename(name)
+  puts name
+
+  # A,まず、nameに"Tanaka"が代入され、次にrenameメソッドが呼ばれる。
+  # メソッドrename(name)の（name)に"Tanaka"が引数として渡され、"Mr.Tanaka"が新たにnameに代入され、
+  # 返り値と渡される。その返り値を再度変数に代入することでメソッドでの処理結果を利用することができる。
