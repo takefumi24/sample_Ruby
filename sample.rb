0b79af9e02f6d33627194bc1f4c8501f22358292
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-17 11:22:26 +0900

    if else問題

--- sample.rb
+++ sample.rb
@@ -1,33 +1,35 @@
-  def rename(name)
-    name = "Mr.#{name}"
+# あなたは警官です。aとｂ二人の容疑者の取り調べをしています。
+# どちらも証言がTrue、またはFalseであればその証言はTrueです。
+# しかしどちらかがFalseでTrueであればその証言はFalse、と出力するメソッドを作りましょう。
+# 呼び出し方：
+# police_trouble(a, b)
+
+# 出力例：
+# police_trouble(true, false) → False
+# police_trouble(false, false) → True
+# police_trouble(true, true) → True
+
+
+def police_trouble(a,b)
+  if(a==b)
+    puts "True"
+  else
+   puts "False"
+  end
+end
+
+puts police_trouble(true,false)
+puts police_trouble(false,false)
+puts police_trouble(true,true)
+
+
+# 模範回答
+def police_trouble(a, b)
+
+  if a && b || !a && !b
+    puts "True"
+  else
+    puts "False"
   end
 
-  name = "Tanaka"
-  rename(name)
-  puts name
-
-# 上記のコードは、ターミナルに「Mr.Tanaka」と表記されることを意図して
-# 書いていますが、実際はそうなりません。
-
-# その理由を、「スコープ」という用語を用いて説明してください。
-
-# A,変数のスコープという概念があり、スコープ外の変数にはアクセスすることができない
-#   Rubyの場合はメソッドの内側と外側ではスコープが違うという仕様になっている。
-#   そのため「def」で定義されているnameという変数と、defの外側のnameは別物である。
-#   よって、renameメソッドのなかのnameに代入しても、外側のnameに変更はない。
-
-
-
-
-# コードを下記のように修正するとうまくいく理由を、「引数」「返り値」という用語を用いて説明してください。
-  def rename(name)
-    name = "Mr.#{name}"
-  en.
-
-  name = "Tanaka"
-  name = rename(name)
-  puts name
-
-  # A,まず、nameに"Tanaka"が代入され、次にrenameメソッドが呼ばれる。
-  # メソッドrename(name)の（name)に"Tanaka"が引数として渡され、"Mr.Tanaka"が新たにnameに代入され、
-  # 返り値と渡される。その返り値を再度変数に代入することでメソッドでの処理結果を利用することができる。
+end
