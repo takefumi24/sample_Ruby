9694082be06c4228cb4c29193882d7879fd7649f
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-18 11:20:54 +0900

    if,else問題(ドリル61)

--- sample.rb
+++ sample.rb
@@ -1,15 +1,22 @@
-# 任意の文字に対してn番目の文字を消し、
-# その消した文字を出力するメソッドを作りましょう。
-# 呼び出し方
-# missing_char(string, num)
+# 20時から翌朝7時までにオウムに喋られると問題があるのでその場合は「NG」、
+# それ以外は「OK」と出力するメソッドを作成します。
+# オウムが喋る時をture、喋らない時をfalseと入力することにし、時刻も同時に入力します。
+
+# 呼び出し方：
+# parrot_trouble(talking, hour)
 
 # 出力例：
-# missing_char('kitten', 1) → 'itten'
-# missing_char('kitten', 2) → 'ktten'
-# missing_char('kitten', 4) → 'kittn'
+# parrot_trouble(true, 6) → NG
+# parrot_trouble(true, 7) → OK
+# parrot_trouble(false, 6) → OK
+# parrot_trouble(false, 7) → OK
 
-def missing_char(array, n)
-  array.slice!(n)
-  puts array
+def parrot_trouble(talking,hour)
+  if (talking && hour < 7 || 20 < hour)
+    puts "NG"
+  else
+    puts "OK"
+  end
 end
-puts missing_char('kitten', 2)
+
+parrot_trouble(true, 6)
