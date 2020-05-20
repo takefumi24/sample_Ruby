f2a79837ee0ad7c6e885c54f3c9be680f6ee6c37
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-20 12:24:02 +0900

    sliceメソッド（64)

--- sample.rb
+++ sample.rb
@@ -1,24 +1,22 @@
-# 1,2,3が全て配列内に入っていれば「True」それ以外は「False」と
-# 出力されるメソッドを作りなさい。
+# 任意の文字列の最初の2文字を最後尾に持ってきて
+# その文字を出力するメソッド
 
 
-
-def array123(nums)
-  if nums.any? {|n| n == 1 && n == 2 && n == 3  }
-    p "True"
-  else
-    p "Flase"
-  end
-end
-array123([1, 1, 2, 3, 1])
-
-解答
-def array123(nums)
-  if (nums.include?(1) && nums.include?(2) && nums.include?(3))
-    puts "True"
-  else
-    puts "False"
-  end
+def left2(str)
+ puts str.slice(2, str.length - 2) + str.slice(0, 2)
 end
 
-array123([1, 1, 2, 3, 1])
+
+def left2(str)
+  puts str[2..-1] + str[0, 2]
+end
+
+left2('Hello')
+left2('java')
+left2('Hi')
+left2('Hihohhokkykky')
+
+# 解答
+# def left2(str)
+#   puts str[2..-1] + str[0..1]
+# end
