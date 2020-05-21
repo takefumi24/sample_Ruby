ac94ff1c9137661955f53e0573a4fa3d0289bf38
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-21 22:11:33 +0900

    文字列変換メソッドについて

--- sample.rb
+++ sample.rb
@@ -1,31 +1,18 @@
-# 下記のコードは、ターミナルに「Mr.Tanaka」と表記されることを意図して
-# 書いていますが、実際はそうなりません。
+下記の（１）を変換して（２）にするためのコードを記述してください。
 
-# その理由を、「スコープ」という用語を用いて説明してください。
+（１） "Today" " is" " sunny" （２） "Today is sunny"
 
-#   def rename(name)
-#     name = "Mr.#{name}"
-#   end
+"Today" + " is" + " sunny"
 
-#   name = "Tanaka"
-#   rename(name)
-#   puts name
-⑴全ファイルに反映させたい内容を記載する。
+（１） "Ruby, Rails, JavaScript" （２） ["Ruby", " Rails", " JavaScript"]
 
-⑵セキュリティ強化し、ストロングパラメーターが必要になる。
-createとupdate自動に割り振れる
+"Ruby, Rails, JavaScript".split(",")
 
-⑶require treeによって全てのファイルが読み込める。
+（１） "rails" （２） "Rails"
+"rails".capitalize
 
-# 模範解答
-⑴デフォルトでは、全てのビューが読み込まれるときapplication.html.erbが読み込まれる。そのため、ヘッダーやフッターなど共通で表示させたい内容をここに記述する。
+（１） "abcde" （２） "edcba"
+"abcde".reverse
 
-⑵
-・form_withで自動でパスを選択してくれて、HTTPメソッドを指定する必要が無い
-・コントローラから渡された、ActiveRecordを継承するモデルのインスタンスが利用できる
-・form_withではinputタグは用いない　　
-
-⑶
-application.html.erbにstylesheet_link_tag 'application'という記述があるため、 app/assets/stylesheets/application.cssが読み込まれる。
-
-application.cssにあるrequire tree .　という記述により、同じフォルダにあるcssファイル全てが読み込まれる。
+（１） "Hello, world" （２） "HELLO, WORLD"
+"Hello, world".upcase
