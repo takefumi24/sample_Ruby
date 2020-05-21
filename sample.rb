029af1dc046e744bc7b6e8d1fd86f1fd8c13b37b
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-21 09:59:55 +0900

    ワンライナー,fizzbuzz(elsif使用禁止)

--- sample.rb
+++ sample.rb
@@ -1,46 +1,57 @@
-# 対話型のインターフェイスで二桁以上の整数を入力すると、十の位と一の位の数字に対して、足す、掛けるを行い、最後にそれぞれの結果を足し合わせて出力するプログラムをrubyで作成
-def add(a, b)
-  a + b
+# 1,以下のコードの２行目以降を、ワンライナーで書き直してください。
+
+str = "is this a pen?"
+if str.include?("?")
+  puts "yes, it is."
 end
 
-def multi(a,b)
-  a * b
+puts "yes, it is." if str.include?("?")
+
+
+# 2,fizzbuzzと言われる問題があります。これは、1から100まで数を数える時に、3の倍数なら「fizz」、5の倍数なら「buzz」、ただし15の倍数なら「fizzbuzz」、それ以外はその数を答えるものです。
+
+# 例）
+# 1
+# 2
+# Fizz
+# 4
+# .
+# .
+
+# これを実現するコードを記述してください。
+# ただし、elsifは利用使用しないものとします。
+for num in 1..100 do
+  case
+    when num % 5 == 0 && num % 3 == 0
+      puts "fizzbuzz"
+    when num % 5 == 0
+      puts "fizz"
+    when num % 3 == 0
+      puts "buzz"
+  else
+    puts num
+  end
 end
 
-def slice(num)
-  top = (num / 10).round
-  bottom = num - top
-  return top, bottom
+
+# 1,模範解答
+str = "is this a pen?"
+puts "yes, it is." if str.include?("?")
+
+# 2,模範解答
+num = 1
+while num < 101
+  str = ""
+  if num % 3 == 0
+    str = str + "fizz"
+  end
+
+  if num % 5 == 0
+    str = str + "buzz"
+  end
+  if str == ""
+    str = str + num.to_s
+  end
+  puts str
+  num += 1
 end
-
-puts "二桁の整数を入力してください"
-input = gets.to_i
-X, Y = slice(input)
-add_result = addition(X, Y)
-multiple_result = multiplication(X, Y)
-puts "足し算結果と掛け算結果の合計値は#{add_result + multiple_result}です"
-
-
-# 模範解答
-def addition(a, b)
-  a + b
-end
-
-def multiplication(a,b)
-  a * b
-end
-
-def slice_num(num)
-  # 10の位
-  tens_place = (num / 10) % 10
-  # 1の位
-  ones_place = (num / 1) % 10
-  return tens_place, ones_place
-end
-
-puts "二桁の整数を入力してください"
-input = gets.to_i
-X, Y = slice_num(input)
-add_result = addition(X, Y)
-multiple_result = multiplication(X, Y)
-puts "足し算結果と掛け算結果の合計値は#{add_result + multiple_result}です"
