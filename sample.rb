ca1eae0dea8d4407894f7ca8256221cfaa20d0d1
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-21 21:34:04 +0900

    スコープの概念について

--- sample.rb
+++ sample.rb
@@ -1,13 +1,31 @@
-# 下記のように直前の2つの数字を足したものを次の数字とする数列がある場合、
-# 20項目目の数字は何になるか求めてください。
+# 下記のコードは、ターミナルに「Mr.Tanaka」と表記されることを意図して
+# 書いていますが、実際はそうなりません。
 
-# 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, …
+# その理由を、「スコープ」という用語を用いて説明してください。
 
-def fibonacci(n)
-  if n == 1 || n == 2
-    return 1
-  else
-    return fibonacci(n-1) + fibonacci(n-2)
-  end
-end
-puts fibonacci(20)
+#   def rename(name)
+#     name = "Mr.#{name}"
+#   end
+
+#   name = "Tanaka"
+#   rename(name)
+#   puts name
+⑴全ファイルに反映させたい内容を記載する。
+
+⑵セキュリティ強化し、ストロングパラメーターが必要になる。
+createとupdate自動に割り振れる
+
+⑶require treeによって全てのファイルが読み込める。
+
+# 模範解答
+⑴デフォルトでは、全てのビューが読み込まれるときapplication.html.erbが読み込まれる。そのため、ヘッダーやフッターなど共通で表示させたい内容をここに記述する。
+
+⑵
+・form_withで自動でパスを選択してくれて、HTTPメソッドを指定する必要が無い
+・コントローラから渡された、ActiveRecordを継承するモデルのインスタンスが利用できる
+・form_withではinputタグは用いない　　
+
+⑶
+application.html.erbにstylesheet_link_tag 'application'という記述があるため、 app/assets/stylesheets/application.cssが読み込まれる。
+
+application.cssにあるrequire tree .　という記述により、同じフォルダにあるcssファイル全てが読み込まれる。
