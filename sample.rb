e0b88f5fd2edda2f421d28fee92da74669b12bba
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-22 14:50:29 +0900

    メソッドに含まれないもの

--- sample.rb
+++ sample.rb
@@ -1,4 +1,9 @@
-# 以下のハッシュに対してメソッドを使用し、キーだけを1つの配列に入れてください。
-
-attr = {name: "田中", age: 27, height: 180, weight: 75}
-puts attr.keys
+# 以下のメソッドの中には、Rubyには元々存在しない、Railsとその周辺のライブラリが提供しているメソッドが含まれています。
+# どのようなサイトを参照すればよいかを考え、Rubyには含まれていないメソッドを答えてください。
+# またフィードバック用に、参照したサイトのURLを記入してください。
+# inject, map, try, send, include?, pluck, select, blank?, empty?, presence
+・try
+・pluck
+・blank?
+・presence
+# http://miyamae.github.io/rubydoc-ja/2.4.0/#!/doc/index.html
