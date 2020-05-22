9d103cb0de15e259dac2054678910b877a4ef431
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-22 15:10:39 +0900

    rubyのAPI問題(最後の2文字を3回繰り返す)

--- sample.rb
+++ sample.rb
@@ -1,9 +1,22 @@
-# 以下のメソッドの中には、Rubyには元々存在しない、Railsとその周辺のライブラリが提供しているメソッドが含まれています。
-# どのようなサイトを参照すればよいかを考え、Rubyには含まれていないメソッドを答えてください。
-# またフィードバック用に、参照したサイトのURLを記入してください。
-# inject, map, try, send, include?, pluck, select, blank?, empty?, presence
-・try
-・pluck
-・blank?
-・presence
-# http://miyamae.github.io/rubydoc-ja/2.4.0/#!/doc/index.html
+# 任意の文字列の最後の2文字を3回繰り返し
+# 出力するメソッドを作りましょう。
+# 出力例：
+# extra_end('Hello') → 'lololo'
+# extra_end('ab') → 'ababab'
+# extra_end('Hi') → 'HiHiHi'
+
+def extra_end(str)
+  num = str[-2,2]
+  puts num *3
+end
+
+extra_end("Hello")
+extra_end('ab')
+
+# 模範解答
+
+def extra_end(str)
+  char_num = str.length
+  right2 = str.slice!(char_num - 2,2)
+  puts right2*3
+end
