95281bc75adb64e5f5fc5a3693fb35608f9c9f60
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-25 10:13:02 +0900

    曜日を求める(wdayメソッド)

--- sample.rb
+++ sample.rb
@@ -1,12 +1,26 @@
-# 以下のハッシュから値だけを取り出し、配列にしてください。ただし、hashクラスのvaluesメソッドは利用しないこととします。
+# 今日の曜日を表示するコードを記述してください。
+# ただし、金曜日だった場合だけ以下のように表示の内容を変えてください。
 
-attr = {name: "田中", age: 27, height: 180, weight: 75}
-values = attr.map(&:last)
+# require "date"
 
-puts values
+# d = Date.today
+# print(d.year, "年", d.month, "月", d.day, "日")
+# day = Date.today.wday
+# youbi = %w[日 月 火 水 木 金 土]
+# if day == 5
+#   print(youbi[day],"曜日だ！！")
+# else
+#   print(youbi[day],"曜日")
+# end
 
 # 解答
-values = []
-attr.each do |key, value|
-  values << value
+require "date"
+
+day = Date.today.wday
+days = ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"]
+
+if day == 5
+  puts "今日は#{days[day]}だ！！！"
+else
+  puts "今日は#{days[day]}"
 end
