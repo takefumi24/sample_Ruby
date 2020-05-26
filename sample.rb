b9a66914f409141cc682135ae19488d7594ef902
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-26 11:26:37 +0900

    【問題】if,else応用問題

--- sample.rb
+++ sample.rb
@@ -1,8 +1,25 @@
-# 文字列を数字に変換するメソッドを答えなさい。また、そのメソッドを用いて、下記の変数strを文字列から数字に変換してください。
-str = "1"
-puts str.to_i
+# 数値numが1以上10以下の範囲であればTrueを出力します。
+# outside_modeがTrueであった場合は数値が0以下、または11以上であってもTrueを出力します。
+# それ以外はFalseを出力するメソッドを作りましょう。
+def in1to10(int,outside_mode)
+ if int >=1 && int <= 10 || outside_mode == true
+  puts "True"
+ else
+  puts "False"
+ end
+end
 
-# 以下の配列arrayに対してあるメソッドを実行し、最後の値"something"を配列から削除してください。
-array = ["dog", "cat", "something"]
-array.pop
-puts array
+
+in1to10(5,false)
+in1to10(11,false)
+in1to10(11,true)
+
+
+# 解答
+def in1to10(num,outside_mode)
+  if (num >= 1 && num <= 10) || outside_mode
+    puts "True"
+  else
+    puts "False"
+  end
+end
