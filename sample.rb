57b3a894323ab69d4a0b30c6f8d393f7a70d7e20
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-26 10:24:43 +0900

    if→unlessに書き換え問題

--- sample.rb
+++ sample.rb
@@ -1,24 +1,8 @@
-# 正の整数を入力します。その整数が、10の倍数（10,20,30...）からの差が、2以内であるときはTrue
-# それ以外はFalseと出力するメソッドを作りましょう。
+# 次のif文をunlessを用いて書き換えてください。
+# if a + b > 0
+#   puts "計算結果は0より大きいです"
+# end
 
-def near_ten(num)
-  if num % 10 <= 2 || num % 10 >= 8
-    puts "#{num}はTrue"
-  else
-    puts "#{num}はFalse"
-  end
-end
-
-puts "正の整数を入力してください。"
-input = gets.chomp.to_i
-puts near_ten(input)
-
-# 解答
-def near_ten(num)
-  quotient = num % 10
-  if quotient  <= 2 || quotient >= 8
-    puts "True"
-  else
-    puts "False"
-  end
+unless a+b <= 0
+  puts "計算結果は0より大きいです"
 end
