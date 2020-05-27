4f3acbcfcf0f2b5334a651c72e1857321cfee9e6
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-27 10:50:22 +0900

    【問題】if else応用問題2

--- sample.rb
+++ sample.rb
@@ -1,30 +1,45 @@
-# 以下の配列から、数を探して何番目に含まれているか結果を返すメソッドを作成してください。
-input = [3, 5, 9 ,12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93]
+# 警察がネズミ取りをしています。
+# 速度が60以下の場合、出力は0になります。
+# 速度が61以上80以下の場合、出力は1になります。
+# 速度が81以上の場合、出力は2になります。
+# 出力を計算するためのコードを作成し、
+# int値としてエンコードします。
+# 0=チケットなし、1 =小チケット、2 =大チケット。
+# と出力するメソッドを作りましょう。
+# アタナの誕生日の日だけ速度制限が5倍大きくなります。
+# 速度は小数点以下も有効ですが、判定の際は小数点一桁で四捨五入して整数にしてください。
 
-def search(num,input)
-  if input.include?(num)
-    search_number = input.index(num)
-    puts "#{search_number + 1}番目にあります"
+def caught_speeding(speed, is_birthday)
+  if is_birthday == "True"
+    bonus = 5
   else
-    puts "その数は含まれていません"
+    bonus = 1
+  end
+
+  if speed.to_i > 80 * bonus
+    puts 2
+  elsif speed.to_i > 60 * bonus && speed.to_i <= 80 * bonus
+    puts 1
+  else
+    puts 0
   end
 end
 
-search(12, input)
-search(7, input)
+caught_speeding(60, "False")
+caught_speeding(65, "False")
+caught_speeding(65, "True")
 
 
 # 解答
-def search(target_num, input)
+def caught_speeding(speed, is_birthday)
+  speed_rounded = speed.round(1)
 
-  input.each_with_index do |num, index|
-    if num == target_num
-      puts "#{index + 1}番目にあります"
-      return
-    end
+  if speed_rounded <= 60
+    point = 0
+  elsif speed_rounded <= 80
+    point = 1
+  else
+    point = 2
   end
-  puts "その数は含まれていません"
+  puts is_birthday ? point * 5 : point
 end
-
-input = [3, 5, 9 ,12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93]
-search(11, input)
