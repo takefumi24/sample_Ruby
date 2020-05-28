8e40013cf13aaf5bbfd763ed696cc803c36373f6
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-28 10:00:57 +0900

    ワンライナーへのリファクタリング

--- sample.rb
+++ sample.rb
@@ -1,25 +1,18 @@
-# 三項演算子とはどのようなものか説明してください。
-# 条件分岐文を1行にまとめて書いたもの
+# 同じ結果を得るためにも、様々なコードの記述方法があります。
+# 以下のコードをワンライナーに書き換える方法について、２パターン以上のコードを記述してください。
+array = [1, 2, 3, 4, 5].map do |el|
+  if el.odd?
+    el
+  end
+end.compact!
 
-# 以下の記述を、三項演算子を用いて書き換えてください。
-var = "文字列"
-
-# if var.class == String
-#   "varはStringです"
-# else
-#   "varはStringではありません"
-# end
-
-puts var.class == String ? "varはStringです": "varはStringではありません"
-
+array = [1, 2, 3, 4, 5].map { |el| el if el.odd? }.compact!
+array = (1..5).select(&:odd?)
 
 
 # 解答
-1
-条件分岐を行うための演算子。
-条件式 ? 真の時の値 : 偽の時の値
-条件式を判定し、trueだった時は「真の時の値」を、falseだった時は「偽の時の値」を返す。条件や返り値がシンプルな場合、if分よりも簡潔に記述できる。
-
-2.
-var = "文字列"
-var.class == String ? "varはstringです" : "varはstringではありません"
+（例）
+array = [1, 2, 3, 4, 5].map { |el| el if el.odd? }.compact!
+array = (1..5).to_a.delete_if { |el| el.even? }
+array = (1..5).to_a.delete_if(&:even?)
+array = [1, 2, 3, 4, 5].select{ |el| el.odd?}
