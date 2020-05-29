311195b45e99c5c039f5164add56f7af37faf4d4
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-29 10:09:19 +0900

    引数のバリエーションについて

--- sample.rb
+++ sample.rb
@@ -1,35 +1,27 @@
-# 配列の内部に、複数のユーザーの情報をハッシュとして持つ変数user_dataがあります。
-# user_dataを利用して、全てのユーザーの名前だけが出力されるようにRubyでコーディングしてください。
-# ただし、出力結果は次のようになるものとします。
+# Rubyのメソッドを定義するときに、様々な引数の渡し方ができます。
+# 以下の機能を利用したメソッドのコードを記述してください。
+# メソッドの内容は問いません。
 
-user_data = [
-  {
-    user: {
-        profile: {
-            name: 'George'
-        }
-    }
-  },
-  {
-    user: {
-        profile: {
-          name: 'Alice'
-        }
-    }
-  },
-  {
-    user: {
-        profile: {
-            name: 'Taro'
-        }
-    }
-  }
-]
+# ①デフォルト値
+def name(first_name = "tanaka", last_name = "souta")
+  puts "my name is #{first_name} #{last_name}"
+end
+name("dan")
 
-user_data.each do |data|
-  puts data[:user][:profile][:name]
+
+
+# ②可変長引数
+
+def gse(*numbers)
+  puts "今日の特急は#{numbers.join}です。"
 end
 
+gse("1号","2号","3号","4号")
 
-# 解答
-user_data.each{ |u| puts u.dig(:user, :profile, :name) }
+
+# ③キーワード引数
+def apple(color: "red", price: 200, size:)
+  puts "#{color}, #{price}, #{size}"
+end
+
+apple
