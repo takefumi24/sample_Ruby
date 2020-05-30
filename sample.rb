d4d7c16095da103f0e23cfdf4e3406adff80ae27
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-30 09:37:14 +0900

    timesメソッド

--- sample.rb
+++ sample.rb
@@ -1,51 +1,14 @@
-# 3つの整数a b cが与えられた場合、bまたはcがaとの差が１でかつbとｃとの数値の差が2以上の場合はTrue。
-# それ以外はFalseと出力するメソッドを作りましょう。
+# ユーザーに数字を入力してもらい、その数の回数だけHello!と表示させるコードを記述してください。
 
-def close_far(a, b, c)
-  if ((b - a).abs == 1 || (c - a).abs == 1) && (b - c).abs >= 2
-    puts "True"
-  else
-    puts "False"
-  end
-end
-
-close_far(1, 2, 10)
-close_far(1, 2, 3)
-close_far(4, 1, 3)
-
-
-# 別解
-def close_far(a,b,c)
-  x = (a-b).abs
-  y = (a-c).abs
-  z = (b-c).abs
-
-  if ((x == 1 || y == 1) && z >= 2)
-    puts "True"
-  else
-    puts "False"
-  end
-end
-
-close_far(1, 2, 10)
-close_far(1, 2, 3)
-close_far(4, 1, 3)
-
-
-
-
+puts "数字を入力して下さい"
+input = gets.chomp.to_i
+input.times {|num| puts "Hello"}
 
 # 解答
-def close_far(a,b,c)
-  x = (a-b).abs
-  y = (a-c).abs
-  z = (b-c).abs
-
-  if (x == 1 && z >= 2)
-    puts "True"
-  elsif ( y == 1 && z >= 2)
-    puts "True"
-  else
-    puts "False"
-  end
+def output(num)
+  num.times{ puts "Hello!" }
 end
+
+puts "何回表示させますか？"
+num = gets.to_i
+output(num)
