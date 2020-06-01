2824559b7ab16e8f9de402621861d195cb862fb5
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-01 09:40:24 +0900

    【問題】動的なクラス名定義

--- sample.rb
+++ sample.rb
@@ -1,19 +1,24 @@
-# シーザー暗号と呼ばれる暗号があります。これはアルファベットをある文字数分ずらすという暗号方式で、例えば「a」を２文字分ずらす（進める）と「c」になります。
-# 「frqjudwxodwlrq」という文字列があり、これを３文字ずらす（戻す）と復号できることがわかっています。それを実現させるコードを記述してください。
-chars = [*"a".."z"]
-char = "frqjudwxodwlrq"
-crypted = char.tr(chars.join, chars.rotate(-3).join)
-puts crypted
+# クラスの定義は、一般的には以下の記述が用いられます。
+# class User
+  # ブロックの内部で各種メソッドを定義
+# end
+# しかしながら、この記述法以外にも、Rubyにはクラスを定義する方法が用意されています。
+# ①上記のclass クラス名 end;という記述を用いないで、Tweetというクラスを定義してください。
 
-# 解答
-
-char = "frqjudwxodwlrq"
-char_ary = char.split("")
-
-changed_char_ary = []
-
-char_ary.each do |char|
-  changed_char_ary << (char.ord - 3).chr
+# 問題１
+self.class.const_set :'Tweet', Class.new do
+  # ブロックの内部で各種メソッドを定義
 end
 
-p changed_char_ary.join
+# 問題2
+クラスを動的に定義できる
+
+
+# 解答
+# 問題1
+Tweet = Class.new do
+# ブロックの内部で各種メソッドを定義
+end
+
+# 問題2
+ブロックの外の変数が呼び出せる
