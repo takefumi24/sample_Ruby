39122cca2c4858ad9445a5b61a74c6057ae8afca
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-03 09:48:51 +0900

    【問題】正規表現

--- sample.rb
+++ sample.rb
@@ -1,30 +1,13 @@
-# 3つの整数a b cが与えられた場合、bまたはcがaとの差が１で、かつbとｃとの数値の差が2以上の場合はTrue。
-# それ以外はFalseと出力するメソッドを作りましょう。
-
-def close_far(a,b,c)
-  if ((b - a).abs == 1 || (c - a).abs == 1) && (b - c).abs >= 2
-    puts "True"
-  else
-    puts "False"
-  end
+def count_coxe(str)
+  count = str.scan(/co[a-z]e/).size
+  puts "#{count}回マッチしました"
 end
 
-close_far(1, 2, 10)
-close_far(1, 2, 3)
-close_far(4, 1, 3)
-
+count_coxe('aaacodebbb')
+count_coxe('codexxcode')
+count_coxe('cozexxcope')
 
 # 解答
-def close_far(a,b,c)
-  x = (a-b).abs
-  y = (a-c).abs
-  z = (b-c).abs
-
-  if (x == 1 && z >= 2)
-    puts "True"
-  elsif ( y == 1 && z >= 2)
-    puts "True"
-  else
-    puts "False"
-  end
+def count_code(str)
+  puts str.scan(/co.e/).length
 end
