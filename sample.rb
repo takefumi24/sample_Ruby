e3c42822f2bac5c16fe0bf64bf92a77ec3e9d965
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-04 20:09:57 +0900

    splitメソッド

--- sample.rb
+++ sample.rb
@@ -1,34 +1,7 @@
-# 任意の3つの数値abcの合計を出力するメソッドを作りましょう。
-# ただし、同じ数が含まれている場合、合計にはカウントされません。
 
+# 下記の（１）を変換して（２）にするためのコードを記述してください。
+# （１） "Ruby, Rails, JavaScript"
+# （２） ["Ruby", " Rails", " JavaScript"]
 
-def lone_sum(a, b, c)
-  sum = 0
-  numbers = a, b, c
-  numbers.each do |num|
-    if numbers.count(num) == 1
-      sum += num
-    end
-  end
-  p sum
-end
-
-lone_sum(1, 2, 3)
-lone_sum(3, 2, 3)
-lone_sum(3, 3, 3)
-
-# 解答
-def lone_sum(ary)
-  # 1. 配列から、重複する要素を取り出す
-  un_uniqs = []
-  ary.each_with_index do |n, i|
-    cp_ary = ary.drop(i+1)
-    cp_ary.each do |cn|
-      un_uniqs << n if n == cn
-    end
-  end
-  # 2. 重複した要素をuniqする
-  uniq_un_uniqs = un_uniqs.uniq
-  # 3. 重複する数字と元の配列の数字を比較し、もし重複する数字でなければ足し合わせる
-  ary.inject(0) {|n, item| uniq_un_uniqs.include?(item) ? n + 0 : n + item }
-end
+strs = "Ruby,Rails,JavaScript"
+puts strs.split(",")
