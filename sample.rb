bfa87dc4526bec21cf2448db73a0fc24c3ad7f4a
takefumi24 <t_ohba24@yahoo.co.jp>
2020-07-03 08:17:45 +0900

    Update sample.rb

--- sample.rb
+++ sample.rb
@@ -1,15 +1,1 @@
-# 任意の文字列で"hi"がいくつあるか数えてその数を出力するメソッドを作りましょう。
-
-def count_hi(str)
-  puts str.scan("hi").length
-end
-
-count_hi('abc hi ho')
-count_hi('ABChi hi')
-count_hi('hihi')
-
-
-# 解答
-def count_hi(str)
-  puts str.scan("hi").length
-end
+n = 2
